34d8756f682bff57bbad2baf899373a2a943d0be
sachin123jadhav <sachin123jadhav@gmail.com>
2025-03-20 12:12:52 +0530

    complete 1

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ export default function App() {
           <WatchedMovieList watched={watched} />
         </Box>
 
-        <StarRating />
+        <StarRating maxRating={5} />
       </Main>
     </>
   );
